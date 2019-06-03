32e8fb21dd704e2e7ac9f2fe43d9398d4efbc250
Fraser McLean <voltboyee@gmail.com>
2019-06-03 16:36:50 +1000

    Removed old SSHClient

--- SSHClient/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SSHClient")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SSHClient")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
