676a190a6109b6b2418d106040a7b196148d4c6c
Fraser McLean <voltboyee@gmail.com>
2019-06-04 10:40:03 +1000

    Created new project for SimplSshClient

---
+++ SimplSshClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SimplSshClient")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimplSshClient")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
---
+++ SimplSshClient/SimplSshClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using FM.SSH;
+
+namespace FM.SSH
+{
+    public class SimplSshClient : SshClientManager
+    {
+        #region Constructor
+        public SimplSshClient()
+        {
+        }
+        #endregion
+
+        #region Public methods
+        public void Initialize()
+        {
+            base.Initialize(debug, hostname, port, username, password);
+        }
+        public new void Connect()
+        {
+            base.Connect();
+        }
+        public new void Disconnect()
+        {
+            base.Disconnect();
+        }
+        public new void Send(string s)
+        {
+            base.Send(s);
+        }
+        #endregion
+    }
+}
